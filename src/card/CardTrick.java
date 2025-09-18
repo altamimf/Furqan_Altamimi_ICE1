490d0a316df33b00002090a4395d1ac76ba140e1
altamimf <altamimf@sheridancollege.ca>
2025-09-18 13:17:07 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -58,24 +58,7 @@ public class CardTrick {
                 System.out.println("Sorry, your card is not in the magic hand.");
             }
  
-            // lucky card (hard-coded)
-            Card luckyCard = new Card();
-            luckyCard.setValue(2);
-            luckyCard.setSuit(Card.SUITS[2]); // 2 = Clubs
- 
-            boolean luckyFound = false;
-            for (Card c : magicHand) {
-                if (c.getValue() == luckyCard.getValue() && c.getSuit().equals(luckyCard.getSuit())) {
-                    luckyFound = true;
-                    break;
-                }
-            }
- 
-            if (luckyFound) {
-                System.out.println("You WIN! The lucky card (Clubs 2) is in the magic hand!");
-            } else {
-                System.out.println("The lucky card (Clubs 2) is not in the magic hand.");
-            }
+            
         }
     }
 }
