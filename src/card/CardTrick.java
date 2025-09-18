d2229b6cb8d7f0c0a35f10ea4e5fd37411c161b8
altamimf <altamimf@sheridancollege.ca>
2025-09-18 13:23:28 -0400

    Update1 CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  *
- * Modifier: Your Name - StudentID
+ * Modifier: Furqan Altamimi - StudentID 991729893
  * Date: 2025-09-17
  */
 public class CardTrick {
